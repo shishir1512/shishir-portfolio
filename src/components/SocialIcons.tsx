63d4824c89ffa6f88d21b6a90033ff693eb528f4
Shishir KB <shishirkb1512@gmail.com>
2026-07-20 01:28:06 +0530

    Add web resume resume.html and upgrade color palette with highly vibrant gradients and glows

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -80,7 +80,7 @@ const SocialIcons = () => {
           </a>
         </span>
       </div>
-      <a className="resume-button" href="#" data-cursor="disable">
+      <a className="resume-button" href="/resume.html" target="_blank" data-cursor="disable">
         <HoverLinks text="RESUME" />
         <span>
           <TbNotes />
