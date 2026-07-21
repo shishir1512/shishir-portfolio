294cb4423e548e6545fb378c608ebcd7caab737a
Shishir KB <shishirkb1512@gmail.com>
2026-07-21 10:43:34 +0530

    Deploy static site with glossy white 3D spheres and fixed texture loader

--- assets/Navbar-We9rJXAc.js
+++ assets/Navbar-We9rJXAc.js
@@ -1,4 +1,4 @@
-import{j,r as bt}from"./index-C2zUx7eM.js";import{g as St,S as yt}from"./ScrollTrigger-CiEuWA-R.js";/*!
+import{j,r as bt}from"./index-Bhsx7HiI.js";import{g as St,S as yt}from"./ScrollTrigger-CiEuWA-R.js";/*!
  * SplitText 3.15.0
  * https://gsap.com
  *
