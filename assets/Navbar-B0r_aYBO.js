09a10ba52aeb59a7b3c29f13350bf9a64b9f504f
Shishir KB <shishirkb1512@gmail.com>
2026-07-21 09:20:56 +0530

    Deploy static site with updated section title TOOLS & SKILLS

--- assets/Navbar-B0r_aYBO.js
+++ assets/Navbar-B0r_aYBO.js
@@ -1,4 +1,4 @@
-import{j,r as bt}from"./index-LF0-5YzD.js";import{g as St,S as yt}from"./ScrollTrigger-CiEuWA-R.js";/*!
+import{j,r as bt}from"./index-CRwdzYPX.js";import{g as St,S as yt}from"./ScrollTrigger-CiEuWA-R.js";/*!
  * SplitText 3.15.0
  * https://gsap.com
  *
